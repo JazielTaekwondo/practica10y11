0e7a22e8dd62238c0750de5f61b9a3e73f900755
Rodrigo <rodrigo.lugofi@gmail.com>
2023-11-19 21:22:48 -0600

    Version final 2
    Ya crea el archivo regalos.txt donde solo aparecen los 12 regalos

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
         System.out.println("\nARCHIVO LEIDO EN PANTALLA:\n");
         alfa.leerArchivo();
         alfa.crearArchivo();
+        alfa.crearArchivoRegalos();
         System.out.println("Puede revisar la secuencia solicitada en secuencia.txt");
+        System.out.println("Puede revisar el archivo con los 12 regalos en regalos.txt");
     }
 }
--- poo/paquete/texto/Secuencia.java
+++ poo/paquete/texto/Secuencia.java
@@ -5,16 +5,21 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Secuencia {
     private Stack<String> pila = new Stack<>();
     private String rutaVillancico;
     private String rutaSecuencia;
+    private String rutaRegalos;
 
     public Secuencia(){
         String directorioActual = System.getProperty("user.dir");// Obtiene la ruta del directorio actual
         this.rutaVillancico = directorioActual + "/12_dias.txt";
         this.rutaSecuencia = directorioActual + "/secuencia.txt";
+        this.rutaRegalos = directorioActual + "/regalos.txt";
     }
 
     public void crearArchivo(){
@@ -25,12 +30,68 @@ public class Secuencia {
                 fw.write(this.pila.pop());
             }
             fw.close();
-            System.out.println("\nArchivo creado con exito");
+            System.out.println("\nArchivo de secuencia creado con exito");
 
         }catch(IOException e){
             System.out.println("Error al crear un nuevo archivo: "+ e.getMessage());
         }
     }
+
+    public void crearArchivoRegalos() {
+        List<String> lineas = leerArchivoSecuencia();
+        List<String> REGALOS = new ArrayList<>(Arrays.asList("drummers", "pipers", "lords", "ladies", "maids", "swans",
+                "geese", "rings", "birds", "hens", "doves", "partridge"));
+    
+        try (FileWriter fw = new FileWriter(rutaRegalos)) {
+            fw.write("The gifts are:\n");
+    
+            for (String linea : lineas) {
+                if (contieneRegalos(linea, REGALOS)) {
+                    fw.write(linea + "\n");
+                }
+            }
+            fw.close();
+            System.out.println("Archivo de regalos creado con éxito");
+        } catch (IOException e) {
+            System.out.println("Error al crear un nuevo archivo de regalos: " + e.getMessage());
+        }
+    }
+    
+    private boolean contieneRegalos(String linea, List<String> regalos) {
+        List<String> regalosEncontrados = new ArrayList<>();
+    
+        for (String regalo : regalos) {
+            if (linea.toLowerCase().contains(regalo)) {
+                regalosEncontrados.add(regalo);
+            }
+        }
+        // Crear una nueva lista antes de realizar removeAll
+        List<String> regalosactualizados = new ArrayList<>(regalos);
+        regalosactualizados.removeAll(regalosEncontrados);
+        // Actualizar la lista original con la nueva lista (se quita el regalo que ya se ha encontrado)
+        regalos.clear();
+        regalos.addAll(regalosactualizados);
+        return !regalosEncontrados.isEmpty();
+    }
+
+    public List<String> leerArchivoSecuencia(){
+        List<String> lineas = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader(rutaSecuencia);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linea;
+            while ((linea = bufferedReader.readLine()) != null) {
+                //System.out.println(linea);
+                lineas.add(linea);
+            }
+            bufferedReader.close(); 
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+        }
+        return lineas;
+    }
+
     public void leerArchivo(){
         String linea, parrafo="";
 
